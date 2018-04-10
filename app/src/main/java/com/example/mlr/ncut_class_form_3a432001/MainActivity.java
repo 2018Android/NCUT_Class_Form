5eedb7abc405fa42fdd96324af9975be2b3f79c5
chenyirou <annie851216@gmail.com>
2018-04-10 20:51:52 +0800

    init commit

---
+++ app/src/main/java/com/example/mlr/ncut_class_form_3a432001/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.mlr.ncut_class_form_3a432001;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
